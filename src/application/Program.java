779cff27f3374db4f46b153ccc3d7d237b476239
Luciano Holanda Porto <lug.dba@gmail.com>
2021-03-22 20:54:16 -0300

    UsaInterestService

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import services.BrazilInterestService;
+import services.UsaInterestService;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -17,7 +18,8 @@ public class Program {
         System.out.print("Months: ");
         int months = sc.nextInt();
 
-        BrazilInterestService is = new BrazilInterestService(2.0);
+        //BrazilInterestService is = new BrazilInterestService(2.0);
+        UsaInterestService is = new UsaInterestService(1.0);
         double payment = is.payment(amount, months);
 
         System.out.println("Payment after " + months + " months:");
