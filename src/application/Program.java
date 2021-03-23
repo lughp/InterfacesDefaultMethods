b120ab64ab75d4522bfe523d265a60fd717dfe55
Luciano Holanda Porto <lug.dba@gmail.com>
2021-03-22 21:01:17 -0300

    Interface InterestService with default

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import services.BrazilInterestService;
+import services.InterestService;
 import services.UsaInterestService;
 
 import java.util.Locale;
@@ -18,8 +19,7 @@ public class Program {
         System.out.print("Months: ");
         int months = sc.nextInt();
 
-        //BrazilInterestService is = new BrazilInterestService(2.0);
-        UsaInterestService is = new UsaInterestService(1.0);
+        InterestService is = new BrazilInterestService(2.0);
         double payment = is.payment(amount, months);
 
         System.out.println("Payment after " + months + " months:");
